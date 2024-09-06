3d8b376c6d513c78f4998ffb4f7942107ce0c81d
MiraiX <117575492+666hwll@users.noreply.github.com>
2024-09-06 18:34:00 +0200

    init.lua aktualisieren

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,7 +7,7 @@ o.relativenumber = true
 o.linebreak = true
 o.showmode = true
 o.lazyredraw = true
-o.cursorline=true
+o.cursorline = true
 o.mouse = "a"
 o.cmdheight = 2
 o.autoindent = true
@@ -18,3 +18,18 @@ opt.clipboard:append {"unnamed", "unnamedplus"}
 
 g.loaded_ruby_provider = 0
 g.loaded_perl_provider = 0
+
+-- Tastenkombinationen für Strg-X, Strg-C und Strg-V
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Text ausschneiden mit Strg-X
+map('v', '<C-x>', '"+x', opts)
+
+-- Text kopieren mit Strg-C
+map('v', '<C-c>', '"+y', opts)
+
+-- Text einfügen mit Strg-V
+map('i', '<C-v>', '<C-r>+', opts)
+map('n', '<C-v>', '"+p', opts)
+map('v', '<C-v>', '"+p', opts)
